9a1714d1ca510e7cd13ae9f7c7445a20bd29a6f6
ashaheer-svg <ashaheer@gmail.com>
2026-02-17 15:57:25 +0530

    Serives bugfix

--- active-hardware-ims/components/services/ServiceRenewalModal.tsx
+++ active-hardware-ims/components/services/ServiceRenewalModal.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog" // Assuming these exist, or I'll use simple div if not
+
 import { X } from "lucide-react"
 
 interface ServiceRenewalModalProps {
